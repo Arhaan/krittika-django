5d69a3122fed28d452efda03b972dc5d68dbdbdd
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-11-29 00:11:11 +0530

    Properly read envvars

--- krittika/settings.py
+++ krittika/settings.py
@@ -18,12 +18,12 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    config = dotenv.dotenv_values(dotenv_file)
+# dotenv_file = os.path.join(BASE_DIR, ".env")
+# if os.path.isfile(dotenv_file):
+#     config = dotenv.dotenv_values(dotenv_file)
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config[ "SECRET_KEY" ]
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
